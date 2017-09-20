7329b0fc520db636d9c0df7a8fc68388321919c7
Salvador Ariza <sariza@thoughtworks.com>
2017-09-20 18:22:46 -0500

    Completed popular languages on center

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,17 +1,38 @@
 var React = require('react');
 
 class Popular extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    };
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(language){
+    this.setState(function(){
+      return {
+        selectedLanguage: language
+      }
+    });
+  }
+
+
   render() {
     var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
     return(
       <ul className='languages'>
         {languages.map(function(lang){
           return (
-            <li key = {lang}>
+            <li
+              style = {(lang === this.state.selectedLanguage) ? {color : '#d0021b'} : null }
+              key = {lang}
+              onClick = {this.updateLanguage.bind(null,lang)}>
               {lang}
             </li>
           )
-        })}
+        }, this)}
       </ul>
     );
   }
