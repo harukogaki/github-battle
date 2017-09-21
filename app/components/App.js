b8523ce10d7fb900e65ad6d60f08da6beb6594e9
Salvador Ariza <sariza@thoughtworks.com>
2017-09-21 16:28:48 -0500

    Used React Router to route components to actual paths displayed them on the app

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,20 @@
 var React = require('react');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+
+var Nav = require('./Nav');
 var Popular = require('./Popular');
 
 class App extends React.Component {
   render() {
     return (
-      <Popular />
+      <Router>
+          <div className='container'>
+            <Nav/>
+            <Route path='/popular' component = {Popular}/>
+          </div>
+      </Router>
     )
   }
 }
